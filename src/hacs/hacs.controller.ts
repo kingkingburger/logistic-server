0e19b2ce06f300d14885cd1e348255992c4bd5ff
wmh <dnjsalsgh123@gmail.com>
2024-03-25 11:07:19 +0900

    refactoring: hacs 리팩토링

--- src/hacs/hacs.controller.ts
+++ src/hacs/hacs.controller.ts
@@ -3,11 +3,4 @@ import { HacsService } from './hacs.service';
 import { CreateHacsDto } from './dto/create-hacs.dto';
 
 @Controller('hacs')
-export class HacsController {
-  constructor(private readonly hacsService: HacsService) {}
-
-  // @Post()
-  // create(@Body() createHacDto: CreateHacsDto) {
-  //   return this.hacsService.create(createHacDto);
-  // }
-}
+export class HacsController {}
